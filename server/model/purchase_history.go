3f5b771d002e11d56f585b6931ef84596b3d3528
pirotyyy <hiroki1423@docomo.ne.jp>
2024-09-26 15:06:50 +0900

    :adhesive_bandage: modelのコード修正

--- server/model/purchase_history.go
+++ server/model/purchase_history.go
@@ -10,20 +10,20 @@ type PurchaseHistory struct {
 	ID              uuid.UUID `db:"id"`
 	UserID          uuid.UUID `db:"user_id"`
 	ProductID       uuid.UUID `db:"product_id"`
+	ProductName     string    `db:"product_name"`
 	Quantity        int       `db:"quantity"`
 	PriceAtPurchase float64   `db:"price_at_purchase"`
 	PurchaseDate    time.Time `db:"purchase_date"`
-	CreatedAt       time.Time `db:"created_at"`
 }
 
-func (repo *Repository) GetPurchaseHistoryByUserID(userID uuid.UUID) ([]PurchaseHistory, error) {
+func (repo *Repository) GetPurchaseHistoriesByUserID(userID uuid.UUID) ([]PurchaseHistory, error) {
 	var purchases []PurchaseHistory
-	err := repo.db.Select(&purchases, "SELECT * FROM purchase_history WHERE user_id = ?", userID)
+	err := repo.db.Select(&purchases, "SELECT * FROM purchase_history JOIN products ON purchase_history.product_id = products.id WHERE user_id = ?", userID)
 	return purchases, err
 }
 
-func (repo *Repository) CreatePurchase(purchase PurchaseHistory) error {
+func (repo *Repository) CreatePurchase(id string, userID string, productID string, quantity int, priceAtPurchase float64, purchaseDate time.Time, createdAt time.Time) error {
 	_, err := repo.db.Exec("INSERT INTO purchase_history (id, user_id, product_id, quantity, price_at_purchase, purchase_date, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
-		purchase.ID, purchase.UserID, purchase.ProductID, purchase.Quantity, purchase.PriceAtPurchase, purchase.PurchaseDate, purchase.CreatedAt)
+		id, userID, productID, quantity, priceAtPurchase, purchaseDate, createdAt)
 	return err
 }
